18371e4829c19bc9cbcc26595973bfdcafd78289
jtclayt <jt.clayton92@yahoo.com>
2022-02-18 17:12:20 -0800

    Couple of documents here

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wordle Clone
+
+This could be a wordle clone someday if I decide to finish it.
+My wife and I like the game but it sucks waiting 24 hours to keep playing.
---
+++ index.js
@@ -0,0 +1,161 @@
+"use strict";
+(function() {
+  // Apparently this is what the pros do.
+  window.addEventListener('load', init);
+
+  // Just some game params maybe someday it will use an API
+  let currentGuess = "";
+  let word = "words";
+  let currentGuessNumber = 1;
+  let grid = [];
+
+  /** Does the setup things. */
+  function init() {
+    currentGuessNumber = 1;
+    currentGuess = "";
+    grid = [];
+    document.onkeydown = getLetterListener;
+    resetGameBoard();
+  }
+
+  /** Get this party started. */
+  function resetGameBoard() {
+    const gameBoard = id("gameboard");
+
+    while (gameBoard.firstChild) {
+      gameBoard.removeChild(gameBoard.firstChild);
+    }
+
+    for (let row = 0; row < 6; row++) {
+      const currentRow = createRow();
+      grid.push([]);
+
+      for (let col = 0; col < 5; col++) {
+        const block = createBlock();
+        grid[row].push(block);
+        currentRow.append(block);
+      }
+
+      gameBoard.append(currentRow);
+    }
+  }
+
+  /**
+   * This seems optimum.
+   * @returns A rectangle for all your blocks.
+   */
+  function createRow() {
+    const row = gen("section");
+    row.classList.add("row");
+    row.classList.add("justify-content-center");
+    return row;
+  }
+
+  /**
+   * Probably could just be CSS.
+   * @returns Not actually a block.
+   */
+  function createBlock() {
+    const block = gen("p");
+    block.classList.add("block");
+    block.classList.add("border");
+    block.classList.add("border-secondary");
+    block.classList.add("border-3");
+    block.classList.add("rounded");
+    block.classList.add("bg-gradient");
+    block.classList.add("text-center");
+    block.classList.add("fs-1");
+    return block;
+  }
+
+  /**
+   * It definitely knows the alphabet.
+   * @param {event} e When stuff happens.
+   */
+  function getLetterListener(e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      if (currentGuess.length < 5) {
+        grid[currentGuessNumber - 1][currentGuess.length].textContent = e.key.toUpperCase();
+        currentGuess += e.key;
+      }
+    } else if (e.keyCode === 13) {
+      if (currentGuess.length == 5) {
+        submitWord();
+      }
+    } else if (e.keyCode === 8) {
+      if (currentGuess.length > 0) {
+        currentGuess = currentGuess.slice(0, currentGuess.length - 1);
+        console.log(currentGuess);
+        grid[currentGuessNumber - 1][currentGuess.length].textContent = "";
+      }
+    }
+  }
+
+  /** Thinks pretty hard about words and stuff. */
+  function submitWord() {
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === currentGuess[i]) {
+        grid[currentGuessNumber - 1][i].classList.add("bg-success");
+      } else if (word.indexOf(currentGuess[i]) !== -1) {
+        grid[currentGuessNumber - 1][i].classList.add("bg-warning");
+      }
+    }
+
+    if (word === currentGuess || currentGuessNumber === 6) {
+      onEndGame(word === currentGuess);
+    }
+
+    currentGuessNumber++;
+    currentGuess = "";
+  }
+
+  /**
+   * We are in the endgame now.
+   * @param {boolean} isWinner Wowie you could be a winner.
+   */
+  function onEndGame(isWinner) {
+    document.onkeydown = () => {};
+
+    setTimeout(() => {
+      if (isWinner) {
+        alert("congrats");
+      } else {
+        alert("you suck")
+      }
+    }, 500)
+
+    setTimeout(() => {
+      init();
+    }, 2000)
+  }
+
+  /**
+   * Check whether fetch returned a status of 200 OK, throw an error if not.
+   * @param {object} response - The response object from the API for the GET request.
+   * @returns {object} Returns the response if ok, otherwise throws error.
+   */
+   function checkStatus(response) {
+    if (response.ok) {
+      return response;
+    }
+    throw Error("Error in request: " + response.statusText);
+  }
+
+  /**
+   * Returns the a newly created DOM element of given tag.
+   * @param {string} tagName - HTML tag to be created.
+   * @returns {object} - DOM object of new element.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} elId - element ID.
+   * @returns {object} - DOM object associated with id.
+   */
+  function id(elId) {
+    return document.getElementById(elId);
+  }
+}());
