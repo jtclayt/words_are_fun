470ea21633aa0c99fa442585db521e8b38cd6869
jtclayt <jt.clayton92@yahoo.com>
2022-02-21 21:18:27 -0800

    X is a letter

--- src/public/index.js
+++ src/public/index.js
@@ -16,7 +16,7 @@
   const KEYBOARD_LAYOUT = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter"],
-    ["Z", "C", "V", "B", "N", "M", "Backspace"]
+    ["Z", "X", "C", "V", "B", "N", "M", "Backspace"]
   ]
 
   /** Does the setup things. */
